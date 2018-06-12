d5d7b7a8c9460b9321fc3cea430c3f9c83ee3da1
Pengyu <rujoyce95@gmail.com>
2018-06-12 00:40:32 -0400

    commit

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ import (
 	"context"
 	"cloud.google.com/go/storage"
 	"io"
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+
 )
 
 
@@ -40,7 +44,7 @@ const (
 	ES_URL = "http://104.197.103.176:9200"
 	BUCKET_NAME = "post-images-20650200"
 )
-
+var mySigningKey = []byte("secret")
 
 func main() {
 	// Create a client
@@ -77,9 +81,25 @@ func main() {
 
 
 	fmt.Println("started-service")
-	http.HandleFunc("/post", handlerPost)
-	http.HandleFunc("/search", handlerSearch)
+
+	r := mux.NewRouter()
+
+	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+		   ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+				  return mySigningKey, nil
+		   },
+		   SigningMethod: jwt.SigningMethodHS256,
+	})
+
+
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+	r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
+	r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+
+	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
@@ -87,6 +107,10 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
    w.Header().Set("Content-Type", "application/json")
    w.Header().Set("Access-Control-Allow-Origin", "*")
    w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+   user := r.Context().Value("user")
+   claims := user.(*jwt.Token).Claims
+   username := claims.(jwt.MapClaims)["username"]
 
 
 	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
@@ -99,7 +123,7 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
 	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
 	p := &Post{
-		   User:    "1111",
+		   User:    username.(string),
 		   Message: r.FormValue("message"),
 		   Location: Location{
 				  Lat: lat,
